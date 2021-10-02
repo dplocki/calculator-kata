83e17f45a7493b653e8943db96f1f01b02520b9a
Dawid Płocki <dawid.plocki@gmail.com>
2021-10-02 17:21:58 +0200

    Unit tests: Add data generator

--- calculator_test.py
+++ calculator_test.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 def calculate(value):
     if value == '':
@@ -6,14 +7,18 @@ def calculate(value):
 
     return int(value)
 
+def generate_int():
+    return random.randrange(0, 99999)
+
 class Calculator(unittest.TestCase):
     def test_empty_string_should_return_zero(self):
         actual = calculate('')
         self.assertEqual(actual, 0)
 
     def test_single_number_return_that_number(self):
-        actual = calculate('23')
-        self.assertEqual(actual, 23)
+        number = generate_int()
+        actual = calculate(str(number))
+        self.assertEqual(actual, number)
 
 if __name__ == '__main__':
     unittest.main()
