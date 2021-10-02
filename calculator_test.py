8fa1b2c3797cfee10d07d2a6af0f7875a7b713cf
Dawid Płocki <dawid.plocki@gmail.com>
2021-10-02 17:05:40 +0200

    Calculator: Single value

--- calculator_test.py
+++ calculator_test.py
@@ -1,12 +1,19 @@
 import unittest
 
 def calculate(value):
-    return 0
+    if value == '':
+        return 0
+
+    return int(value)
 
 class Calculator(unittest.TestCase):
     def test_empty_string_should_return_zero(self):
         actual = calculate('')
         self.assertEqual(actual, 0)
+
+    def test_single_number_return_that_number(self):
+        actual = calculate('23')
+        self.assertEqual(actual, 23)
 
 if __name__ == '__main__':
     unittest.main()
