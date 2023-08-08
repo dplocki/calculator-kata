70a81190e99573d4cc4e8772ecc34712fb1e55be
Ziomioslaw <dawid.plocki@gmail.com>
2023-08-08 17:20:01 +0200

    Separate the token parsing

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,15 +1,21 @@
-from functools import reduce
 import re
+from typing import Iterable, Union
 
 
-def calculate(value: str):
-    if value == "":
+def parse_input_string_to_tokens(
+    input_value: str,
+) -> Iterable[Union[int, str], None, None]:
+    yield from filter(lambda token: token.strip() != "", re.split("(\W)", input_value))
+
+
+def calculate(input_value: str) -> int:
+    if input_value == "":
         return 0
 
     operator = "+"
     result = 0
 
-    for token in filter(lambda token: token.strip() != '', re.split('(\W)', value)):
+    for token in parse_input_string_to_tokens(input_value):
         if token in "-+":
             operator = token
         else:
